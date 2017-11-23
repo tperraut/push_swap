31e274a5d23203bae9715f643e136a1c3d7cac41
tperraut-geronimo <thomas.p@geronimostudios.com>
2017-11-23 13:42:43 +0000

    [ CHECKER ] Remove exec

--- checker/src/checker.c
+++ checker/src/checker.c
@@ -1,11 +1,25 @@
 
 #include "checker.h"
 #include <stdio.h> //DEBUG
+#include <stdlib.h> //DEBUG
 
 int	main(int ac, char **av)
 {
-	int	a[(ac > 1) ? ac - 1 : ac];
+	int	a[ac];
+	int	i;
 
-	printf("%d\n", ac);
+	i = 0;
+	ac--;
+	while (i < ac)
+	{
+		a[i] = atoi(av[i + 1]);
+		i++;
+	}
+	i = 0;
+	while (i < ac)
+	{
+		printf("%d\n", a[i]);
+		i++;
+	}
 	return (1);
 }
